ff0aeda3e5119445c5ef527911e7f457d97b7dd9
Donovan-Domaoan <lucasdomaoan@gmail.comgit config --global user.name Donovan>
2024-07-30 23:23:46 -0700

    Updates

---
+++ precache-manifest.539714483756c7df1407835b38479d41.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73aa184694b2e3c35d63795ce0e0f81b",
+    "url": "/friendsrobo/index.html"
+  },
+  {
+    "revision": "6a752cb00c5719468e29",
+    "url": "/friendsrobo/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "d1364d61a90bcb140713",
+    "url": "/friendsrobo/static/css/main.7cde5ab8.chunk.css"
+  },
+  {
+    "revision": "6a752cb00c5719468e29",
+    "url": "/friendsrobo/static/js/2.68d07191.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/friendsrobo/static/js/2.68d07191.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1364d61a90bcb140713",
+    "url": "/friendsrobo/static/js/main.f6a0b77c.chunk.js"
+  },
+  {
+    "revision": "e5cfc4c5f5d1a4efd9bb",
+    "url": "/friendsrobo/static/js/runtime-main.aade5fa4.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/friendsrobo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
